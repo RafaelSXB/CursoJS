270204570179dcde7db997060c3d5ccbefcbbd28
RafaelXB <rafael.bento1522@gmail.com>
2022-07-30 17:56:54 -0300

    Commit

---
+++ javascript Objetos/aula03 - ObjectDefine/mainProperty.js
@@ -0,0 +1,17 @@
+// ObjectDefine
+function Pessoa(nome, sobrenome, idade){
+    this.nome = nome,
+    this.sobrenome = sobrenome,
+    this.idade = idade,
+    
+    Object.defineProperty(this, 'endereco', {
+        value: 'teste',
+        enumerable: true,
+        writable: false,
+        configurable: false
+    });
+}
+
+const p1 = new Pessoa;
+
+console.log(Object.keys (p1))
